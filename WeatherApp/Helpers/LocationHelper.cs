55f2ce37ca443220563f1ef0c3d27a86b6ce26b5
Sibusiso Ngqondo <sibusiso@bbd.co.za>
2017-09-12 10:31:26 +0200

    Fixed Location issues on LocatioHelper.cs

--- WeatherApp/Helpers/LocationHelper.cs
+++ WeatherApp/Helpers/LocationHelper.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Plugin.Geolocator;
 using Plugin.Geolocator.Abstractions;
+using Xamarin.Forms;
 
 namespace WeatherApp.Helpers
 {
@@ -14,8 +15,20 @@ namespace WeatherApp.Helpers
 			{
 				var locator = CrossGeolocator.Current;
 				locator.DesiredAccuracy = 50;
-				var position = await locator.GetPositionAsync(new TimeSpan(10000));
-				return position;
+				if (Device.RuntimePlatform == Device.Android)
+				{
+					var position = await locator.GetPositionAsync(timeout: new TimeSpan(10000));
+					return position;
+				}
+				if (Device.RuntimePlatform == Device.iOS)
+				{
+					var position = await locator.GetLastKnownLocationAsync();
+					return position;
+				}
+				else
+				{
+					return null;
+				}
 			}
 			catch (Exception e)
 			{
